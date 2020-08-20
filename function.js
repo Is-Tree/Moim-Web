3c175d152756a262096ece163e5c38e27cacb75e
Is_Tree_Home <lsm3645@g.skku.edu>
2020-08-20 15:59:07 +0900

    BackBtn

--- function.js
+++ function.js
@@ -1,14 +1,21 @@
-
+// Setting
+const SLIDE_DURATION = 500;
 
 // Navigation
 
 const pickers = document.querySelectorAll('.nav__picker'),
     nav = document.querySelector('.nav'),
-    sections = document.querySelectorAll('section');
+    sections = document.querySelectorAll('section'),
+    back = document.querySelector('.back');
+
+let state;
 
 function handlePick(event){
     event.preventDefault();
+
+    state = event.target.dataset.id;
     const target = document.querySelector(`.${event.target.dataset.id}`);
+
     sections.forEach( section => {
         if( section !== target){
             section.style.display = 'none';
@@ -22,8 +29,28 @@ function handlePick(event){
         target.classList.add('nav--active'); },
         0);
 
+    setTimeout(function(){
+            back.classList.add('back--active'); },
+        SLIDE_DURATION);
 }
 
+function handleBack(event){
+    event.preventDefault();
+    const target = document.querySelector(`.${state}`);
+
+    target.scrollIntoView({behavior: "smooth"});
+
+    nav.classList.remove('nav--active');
+    target.classList.remove('nav--active');
+    setTimeout( function(){
+            sections.forEach( section => {
+                section.style.display = 'none';
+            });
+        },
+        SLIDE_DURATION);
+
+    back.classList.remove('back--active');
+}
 
 // About > Rotation on Scrolling
 
@@ -43,6 +70,7 @@ function init(){
     pickers.forEach( picker => {
         picker.addEventListener("click", handlePick);
     })
+    back.addEventListener("click", handleBack);
 }
 
 init();
